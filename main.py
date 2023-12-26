051d4eddeb0114e0dc3d3ab5b699fda552feb99c
THINKPAD-MUSABI\Musabii <musabii@live.com>
2023-12-27 00:39:58 +0500

    recognize speech

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import telebot
 import os
 from dotenv import load_dotenv
+from utils.speech import ogg_to_text
 
 load_dotenv() # reload env params from .env to environment
 apikey = os.environ.get("API_KEY") # from .env file
 
 bot = telebot.TeleBot(apikey)
+workdir = os.path.abspath("./");
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -25,5 +27,21 @@ def send_text(message):
 @bot.message_handler(content_types=['sticker'])
 def send_text(message):
     bot.send_message(message.chat.id, 'стикер')
+
+
+@bot.message_handler(content_types=['voice'])
+def send_text(message):
+    bot.send_message(message.chat.id, 'голос')
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    ogg_file_name = 'files/new_file.ogg'
+    ogg_file_path = os.path.join(workdir, ogg_file_name)
+
+    with open(ogg_file_path, 'wb') as new_file:
+        new_file.write(downloaded_file)
+        txt = ogg_to_text(ogg_file_name)
+        print(txt)
+        bot.send_message(message.chat.id, txt)
+        # os.remove(ogg_file_name)
 
 bot.polling()
---
+++ utils/speech.py
@@ -0,0 +1,41 @@
+# importing libraries
+import speech_recognition as sr # numpy also require
+import os
+import soundfile as sf
+
+
+# create a speech recognition object
+r = sr.Recognizer()
+
+def transcribe_audio(path):
+    with sr.AudioFile(path) as source:
+        audio_listened = r.record(source)
+        text = r.recognize_google(audio_listened, language='ru-RU')
+    return text
+
+def ogg_to_wav(ogg_file_path):
+    data, samplerate = sf.read(ogg_file_path)
+    wav_file_path = ogg_file_path + ".wav"
+    sf.write(wav_file_path, data, samplerate)
+    return wav_file_path
+
+def ogg_to_text(ogg_file_path):
+    # workdir = os.path.abspath("./");
+    # ogg_file_path = os.path.join(workdir, file_name)
+
+    wav_file_path = ogg_to_wav(ogg_file_path)
+
+    txt = transcribe_audio(wav_file_path)
+
+    if os.path.exists(wav_file_path):
+        os.remove(wav_file_path)
+
+    return txt
+
+
+# file_name = "files/new_file_ru.ogg"
+
+# txt = ogg_to_text(file_name)
+
+
+# print(txt);
