902697747336b1e3844c46106de3585b972e021e
THINKPAD-MUSABI\Musabii <musabii@live.com>
2023-12-27 11:39:37 +0500

    exception handle

--- main.py
+++ main.py
@@ -31,15 +31,14 @@ def send_text(message):
 
 @bot.message_handler(content_types=['voice'])
 def send_text(message):
-    bot.send_message(message.chat.id, 'голос')
     file_info = bot.get_file(message.voice.file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    ogg_file_name = 'files/new_file.ogg'
-    ogg_file_path = os.path.join(workdir, ogg_file_name)
+    ogg_file_name = 'new_file.ogg'
+    ogg_file_path = os.path.join(workdir,"files",ogg_file_name)
 
     with open(ogg_file_path, 'wb') as new_file:
         new_file.write(downloaded_file)
-        txt = ogg_to_text(ogg_file_name)
+        txt = ogg_to_text(ogg_file_path)
         print(txt)
         bot.send_message(message.chat.id, txt)
         # os.remove(ogg_file_name)
--- utils/speech.py
+++ utils/speech.py
@@ -10,22 +10,35 @@ r = sr.Recognizer()
 def transcribe_audio(path):
     with sr.AudioFile(path) as source:
         audio_listened = r.record(source)
-        text = r.recognize_google(audio_listened, language='ru-RU')
+        text = ""
+        try:
+            text = r.recognize_google(audio_listened, language='ru-RU')
+        except:
+            print(" не удалось распознать")
+            text = " ...не удалось распознать..."
     return text
 
 def ogg_to_wav(ogg_file_path):
-    data, samplerate = sf.read(ogg_file_path)
-    wav_file_path = ogg_file_path + ".wav"
-    sf.write(wav_file_path, data, samplerate)
-    return wav_file_path
+    try:
+        data, samplerate = sf.read(ogg_file_path)
+        wav_file_path = ogg_file_path + ".wav"
+        sf.write(wav_file_path, data, samplerate)
+        return wav_file_path
+    except:
+        raise Exception
+
+
 
 def ogg_to_text(ogg_file_path):
     # workdir = os.path.abspath("./");
     # ogg_file_path = os.path.join(workdir, file_name)
 
-    wav_file_path = ogg_to_wav(ogg_file_path)
+    try:
+        wav_file_path = ogg_to_wav(ogg_file_path)
 
-    txt = transcribe_audio(wav_file_path)
+        txt = transcribe_audio(wav_file_path)
+    except:
+        txt = "ошибка"
 
     if os.path.exists(wav_file_path):
         os.remove(wav_file_path)
